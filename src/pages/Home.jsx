f74384acb6f397ca19433bb1e73002d7352399a4
jorge niglia <jorge.niglia@gmail.com>
2022-05-25 16:36:44 -0300

    Componentes y rutas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import ProductList from "../containers/ProductList";
+import Header from "../components/Header";
 
 const Home = () => {
   return (
-    <div>
-      <h1>React Shop</h1>
-    </div>
+    <>
+      <Header />
+      <ProductList />
+    </>
   );
 };
 
--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Layout from "../containers/Layout";
 import Login from "../containers/Login";
-import RecoveryPassword from "../containers/RecoveryPassword";
 import "../styles/global.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import NotFound from "../pages/NotFound";
+import PasswordRecovery from "../pages/RecoveryPassword";
+import SendEmail from "../pages/SendEmail";
+import NewPassword from "../pages/NewPassword";
+import MyAccount from "../pages/MyAccount";
+import CreateAccount from "../pages/CreateAccount";
+import Checkout from "../pages/checkout";
+import Orders from "../pages/Orders";
 
 const App = () => {
   return (
@@ -13,7 +19,13 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
-        <Route path="recovery-password" element={<RecoveryPassword />} />
+        <Route path="password-recovery" element={<PasswordRecovery />} />
+        <Route path="/send-email" element={<SendEmail />} />
+        <Route path="/new-password" element={<NewPassword />} />
+        <Route path="/account" element={<MyAccount />} />
+        <Route path="/signup" element={<CreateAccount />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/orders" element={<Orders />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Layout>
